5834040ec7f7b6bd630330ebf4da073f9bd0bbbe
CooLNuanfeng <1634898698@qq.com>
2015-07-03 09:33:24 +0000

    step one

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,89 @@
 requirejs.config({
     paths: {
         jquery: 'jquery',
-        mytableDataPage : '../lib/mytableDataPage.min'
+        //mytableDataPage : '../lib/mytableDataPage.min'
     }
 });
 
-define(['mytableDataPage'],function(tab){
-    var mytable = new tab.mytable();
+define(['mytableDataPage'],function(t){
+
+    var mytable = t.mytable("myTableBox",{
+    	theader : [
+    		{"attr" : [{"id":"1"},{'data-name':'ID'}],"txt":'序号',"id" :"userid"},
+    		{"attr" : [{"id":"2"}],"txt":'姓名',"id":"username"},
+    		{"attr" : [{"id":"3"}],"txt":'序号',"id":"userage"}
+    	],
+    	data : [
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23},
+    		{"userid":0,"username":'blue',"userage":23}
+    	]
+
+    });
 
 
-    mytable.alert();
 })
--- javascript/mytableDataPage.js
+++ javascript/mytableDataPage.js
@@ -1,19 +1,121 @@
 define(['jquery'],function($){
 	
-	function mytable(){
+	function Mytable(){
+		this.options = {
+			"theader" : [],
+			"data" : [],
+			"rows" : 5
+		};
+		//this.tableHtml = '';
+	}
+
+	Mytable.prototype = {
+
+		constructor : 'Mytable',
+
+		init : function(id,options){
+
+			this.options = $.extend({},this.options,options);
+
+			this.render(id);
+			
+		},
+		//整体渲染
+		render : function(id){
+
+			var $box = $('#'+id);
+			var strHtml="<div class=\"tableBody\">" +
+						"</div>" +
+						"<div class=\"tablePage\">" +
+							"<select class=\"selectPage\" value=''>" +
+								"<option value=\"5\">5</option>" +
+								"<option value=\"10\">10</option>" +
+								"<option value=\"15\">15</option>" +
+								"<option value=\"30\">30</option>" +
+								"<option value=\"50\">50</option>" +
+							"</select>" +
+							"<div class=\"table_pages\">" +
+							"</div>" +
+						"</div>";
+
+			$box.html(strHtml);
+
+			this.renderTable($box);
+			this.renderSelectPage($box);
+			this.renderPage($box);
+		},
+		//渲染表格
+		renderTable : function($obj){
+
+			var pages = this.getPagesSum();
+			var tableHtml ="";
+
+			$obj.find('.tableBody').html(tableHtml);
+		},
+		//渲染筛选页数
+		renderSelectPage : function($obj){
+			var rows = this.options.rows;
+			var html = '';
+			var arr = [5,10,15,30,50];
+
+			$.each(arr,function(index,value){
+				if(value == rows){
+					html +="<option value="+value+" selected>"+value+"</option>";
+				}else{
+					html+="<option value="+value+">"+value+"</option>"
+				}
+			});
+			
+			$obj.find('.selectPage').html(html);
+		},
+		//渲染分页部分
+		renderPage : function($obj){
+			var pages = this.getPagesSum()+1;
+			var pageNow = 1;
+			var pageHtml='<a href="javascript:;" class="firstPage"></a><a href="javascript:;" class="prevPage"></a>';
+
+			if(pages>6){
+				pageHtml+='<a href="javascript:;" class="active page_row">1</a><a href="javascript:;" class="page_row">2</a><a href="javascript:;" class="page_row">3</a><a href="javascript:;" class="page_row">4</a><a href="javascript:;" class="page_row">5</a><span>...</span><a href="javascript:;" class="page_row">'+pages+'</a>';	
+			}else{				
+				for(var i=1; i<=pages;i++){
+					if(i==pageNow){
+						pageHtml+='<a href="javascript:;" class="active page_row">'+i+'</a>';
+					}else{
+						pageHtml+='<a href="javascript:;" class="page_row">'+i+'</a>';
+					}					
+				}			
+			}
+			pageHtml+='<a href="javascript:;" class="nextPage"></a><a href="javascript:;" class="lastPage"></a>';
+			$obj.find('.table_pages').html(pageHtml);
+
+			this.pageEvent($obj);	
+		},
+
+		//分页按钮相关交互
+		pageEvent : function($obj){
+			$obj.find('.table_pages').on('click','.page_row',function(){
+				$(this).addClass('active').siblings('a').removeClass('active');
+			})
+		},
+
+		//计算每页应显示的行数或个数
+		getPagesSum : function(){
+			var sum = Math.floor(this.options.data.length/this.options.rows);
+
+			return sum;
+		}
 
 	}
 
-	mytable.prototype = {
-		alert : function(){
-			$('#btn').click(function(event) {
-				alert('a');
-			});
-			
-		}
+	function FactoryTable(id,json){
+		var mytab = new Mytable();
+
+		mytab.init(id,json);
+
+		return mytab;
 	}
 
 	return {
-		mytable : mytable
+		mytable : FactoryTable
 	}
 });
